a98848a282c876fb349676f898647979bc211e65
joheiss <josef.heiss@jovisco.de>
2018-09-03 17:48:09 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Build phase
+FROM node:alpine AS builder
+
+WORKDIR '/usr/app'
+COPY package.json .
+RUN npm install
+COPY . .
+
+CMD ["npm", "run", "build:prod"]
+
+# Run phase
+FROM nginx:alpine
+
+WORKDIR /usr/share/nginx/html
+
+COPY --from=builder /usr/app/dist/ng6-app/ .
+
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,50 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import {TestBed, async, ComponentFixture} from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterTestingModule} from '@angular/router/testing';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
+        NoopAnimationsModule,
         RouterTestingModule
       ],
       declarations: [
         AppComponent
       ],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+    fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'ng6-app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it(`should have as title 'Angular 6 with Docker'`, async(() => {
+    fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ng6-app');
+    expect(app.title).toEqual('Angular 6 with Docker');
   }));
   it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+    fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng6-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Angular 6 with Docker!');
+  }));
+  it('should render title in a h2 tag', async(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h2').textContent).toContain('Here are some links:');
   }));
 });
